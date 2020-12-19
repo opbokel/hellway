b8aae0ed99626c2161b59e000206e655670152d4
Octavio Bokel <dobra9@gmail.com>
2020-12-19 21:51:13 +0100

    64 lines update

--- hellway.asm
+++ hellway.asm
@@ -6,9 +6,9 @@
 	org $F000
 	
 ;contants
-ScreenSize = 32;(192)
+ScreenSize = 64;(192)
 CarSize = 4
-TrafficLineCount = 7
+TrafficLineCount = 1
 	
 ;memory	
 Car0Y = $80
@@ -19,30 +19,10 @@ FrameCount1 = $84;
 PF0Cache = $85
 PF1Cache = $86
 PF2Cache = $87
-TrafficOffset0 = $88;
-TrafficOffset1 = $89;
-TrafficOffset2 = $8A;
-TrafficOffset3 = $8B;
-TrafficOffset4 = $8C;
-TrafficOffset5 = $8D;
-TrafficOffset6 = $8E; border
+TrafficOffset0 = $88; Border
+Car0Speed = $89
+Traffic0SpeedAc = $90
 
-TrafficOffset7 = $8F; not used, probably will change background
-
-Car0Speed = $90
-Traffic0SpeedAc = $91
-Traffic1SpeedAc = $92
-Traffic2SpeedAc = $93
-Traffic3SpeedAc = $94
-Traffic4SpeedAc = $95
-Traffic5SpeedAc = $96
-Traffic6SpeedAc = $97
-
-TmpVar0 = $98
-
-
-TrafficCacheStart = $FF - ScreenSize + 1 ;$E0
-TrafficCacheEnd = $FF
 
 ;generic start up stuff...
 Start
@@ -61,32 +41,9 @@ ClearMem
 	LDA #$1C   ;lets go for bright yellow, for the car
 	STA COLUP0
 ;Setting some variables...
-	LDA #30
+	LDA #8
 	STA Car0Y	;Initial Y Position
 		
-
-	LDA #$ff
-	STA $E1
-	STA $E2
-	STA $E3
-	; STA $E4
-	; STA $E5
-	; STA $E6
-	; STA $E7
-	; LDA #$80
-	; STA $E8
-	; STA $E9
-	; STA $EA
-	; STA $EB
-	
-	; LDA #16
-	; STA TrafficOffset0
-	; STA TrafficOffset1
-	; STA TrafficOffset2
-	; STA TrafficOffset3
-	; STA TrafficOffset4
-	; STA TrafficOffset5
-	; STA TrafficOffset6
 	
 	;Traffic colour
 	LDA $32 
@@ -110,28 +67,6 @@ MainLoop
 	LDA #0
 	STA VSYNC 	
 
-
-; for up and down, we INC or DEC
-; the Y Position
-
-	LDA #%00010000	;Down?
-	BIT SWCHA 
-	BNE SkipMoveDown
-	INC Car0Y
-	; INC Car0Y
-	; INC Car0Y
-	; INC Car0Y
-SkipMoveDown
-	; LDA FrameCount0
-	; BNE SkipMoveUp
-	LDA #%00100000	;Up?
-	BIT SWCHA 
-	BNE SkipMoveUp
-	DEC Car0Y
-	; DEC Car0Y
-	; DEC Car0Y
-	; DEC Car0Y
-SkipMoveUp
 
 ; for left and right, we're gonna 
 ; set the horizontal speed, and then do
@@ -162,17 +97,6 @@ IncreaseCar0Speed
 	INC Car0Speed;
 SkipIncreaseCar0Speed
 		
-	; LDX #2 ;number of times will process the line update
-; UpdateLines
-	; CLC
-	; LDA Traffic6SpeedAc
-	; ADC Car0Speed
-	; STA Traffic6SpeedAc
-	; BCC SkipCar6LineUpdate
-	; INC TrafficOffset6
-	; DEX
-	; BNE UpdateLines
-; SkipCar6LineUpdate
 
 	LDY #2
 RepeatUpdateLines ;to be able to rum more than one line at a time
@@ -190,7 +114,7 @@ CarWithMoreSpeed
 	BCC PrepareNextUpdateLoop
 	INC TrafficOffset0-1,X	
 	JMP PrepareNextUpdateLoop
-CarWithLessSpeed
+CarWithLessSpeed ; Not sure if this still can happen
 	LDA TrafficSpeeds-1,X
 	SEC
 	SBC Car0Speed
@@ -206,27 +130,11 @@ PrepareNextUpdateLoop
 	BNE RepeatUpdateLines
 		
 CountFrame	
-	; INC TrafficOffset0
-	; DEC TrafficOffset2
-	; DEC TrafficOffset3
-	; DEC TrafficOffset3
 	INC FrameCount0
 	BNE SkipIncFC1 ;When it is zero again should increase the MSB
 	INC FrameCount1
-	;INC Car0Y
-	; DEC TrafficOffset1
-	; DEC TrafficOffset4
-	; INC TrafficOffset5
 SkipIncFC1
 	
-;moves the trafic, will be dinamic
-	; INC TrafficOffset0
-	; INC TrafficOffset1
-	; INC TrafficOffset1
-	;INC TrafficOffset2
-	;INC TrafficOffset2
-	; DEC TrafficOffset2
-	; INC TrafficOffset3
 	
 ;keep rotating on 0 - screensize - 1
 
@@ -285,117 +193,15 @@ ClearCache ;11
 	STA PF2Cache ; 3
 	STA PF0Cache ; 3
 	
-DrawCar0 ;14 max
-	LDX TrafficOffset0 ;3
-	LDA TrafficCacheStart,X ;4
-	BPL SkipCar0Draw ;2 if car 1 is on, it is always negative	
-	LDA #%11000000 ;2
-	STA PF1Cache	;3
-SkipCar0Draw
 
-DrawCar1 ;19 max
-	LDX TrafficOffset1 ;3
-	LDA TrafficCacheStart,X ;4
-	AND #%01000000 ;2
-	BEQ SkipCar1Draw ;2
-	LDA PF1Cache ;3
-	ORA #%00011000 ;2
-	STA PF1Cache ;3
-SkipCar1Draw
-
-	STA WSYNC ;71
-
-DrawCar2 ;19
-	LDX TrafficOffset2 ;3
-	LDA TrafficCacheStart,X ;4
-	AND #%001000000 ;2
-	BEQ SkipCar2Draw ;2
-	LDA PF1Cache ;3
-	ORA #%00000011 ;2
-	STA PF1Cache ;3	
-SkipCar2Draw
-
-DrawCar3 ;16
-	LDX TrafficOffset3 ;3
-	LDA TrafficCacheStart,X ;4
-	AND #%00010000 ;2
-	BEQ SkipCar3Draw ;2
-	LDA #%00000110 ;2(MSB first) it was two easy...
-	STA PF2Cache ;3	
-SkipCar3Draw
-
-
-DrawCar4 ;19
-	LDX TrafficOffset4
-	LDA TrafficCacheStart,X
-	AND #%00001000
-	BEQ SkipCar4Draw
-	LDA PF2Cache
-	ORA #%00110000 ;(MSB first) it was two easy...
-	STA PF2Cache	
-SkipCar4Draw
-
-DrawCar5 ;19
-	LDX TrafficOffset5
-	LDA TrafficCacheStart,X
-	AND #%00000100
-	BEQ SkipCar5Draw
-	LDA PF2Cache
-	ORA #%10000000 ;(MSB first) it was two easy...
-	STA PF2Cache	
-SkipCar5Draw
 
 	STA WSYNC ;73
+
+
+	;STA WSYNC ;75
 	
-	;12 max cicle pointer
-	DEC TrafficOffset0 ;5/2
-	BPL SkipTrafficOffset0Reset ; 2/2
-	LDA #ScreenSize - 1 ; 2/2
-	STA TrafficOffset0 ;3/2
-SkipTrafficOffset0Reset
-	
-	;12
-	DEC TrafficOffset1
-	BPL SkipTrafficOffset1Reset
-	LDA #ScreenSize - 1
-	STA TrafficOffset1
-SkipTrafficOffset1Reset	
-
-	DEC TrafficOffset2
-	BPL SkipTrafficOffset2Reset
-	LDA #ScreenSize - 1
-	STA TrafficOffset2
-SkipTrafficOffset2Reset
-
-	DEC TrafficOffset3
-	BPL SkipTrafficOffset3Reset
-	LDA #ScreenSize - 1
-	STA TrafficOffset3
-SkipTrafficOffset3Reset
-
-	DEC TrafficOffset4 ;5
-	BPL SkipTrafficOffset4Reset ;2
-	LDA #ScreenSize - 1 ;2
-	STA TrafficOffset4 ;3
-SkipTrafficOffset4Reset
-
-	DEC TrafficOffset5
-	BPL SkipTrafficOffset5Reset
-	LDA #ScreenSize - 1
-	STA TrafficOffset5	
-SkipTrafficOffset5Reset
-
-
-	STA WSYNC ;75
-	
-DrawCar6 ;16
-	; LDX TrafficOffset6 ;3
-	; LDA TrafficCacheStart,X ;4
-	; AND #%00000010 ;2
-	; BEQ SkipCar6Draw ;2
-	; LDA #%01110000 ;2(MSB first) it was two easy...
-	; STA PF0Cache ;3	
-	LDA TrafficOffset6 ;3
+DrawCar0 ;16 Border
+	LDA TrafficOffset0 ;3
 	ASL ;2
 	ASL ;2
 	ASL ;2
@@ -405,11 +211,11 @@ DrawCar6 ;16
 SkipCar6Draw
 	
 	;12
-	DEC TrafficOffset6
-	BPL SkipTrafficOffset6Reset
+	DEC TrafficOffset0
+	BPL SkipTrafficOffset0Reset
 	LDA #ScreenSize - 1
-	STA TrafficOffset6	
-SkipTrafficOffset6Reset;--
+	STA TrafficOffset0	
+SkipTrafficOffset0Reset;--
 	
 
 BeginDrawCar0Block ;21 to EndDrawCar0Block 21 to finish player (never check if start enable if already on, this is the wrse path)
@@ -433,7 +239,7 @@ SkipActivatePlayer ;EndDrawCar0Block
 	
 	STA WSYNC ;49
 	
-	STA WSYNC ;3
+	;STA WSYNC ;3
 
 WhileScanLoop	
 	DEY	;2
@@ -467,13 +273,7 @@ CarSprite
 	;.byte #%00111100
 	
 TrafficSpeeds ;maybe move to ram for dynamic changes and speed of 0 page access
-	.byte #40 ;car 0
-	.byte #80 
-	.byte #120
-	.byte #160
-	.byte #190
-	.byte #220 ;car 5
-	.byte #0 ;car 6 border
+	.byte #0 ; Border
 
 	org $FFFC
 	.word Start
