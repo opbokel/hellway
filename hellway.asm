152410bb01290eebf913a424aebc787686eabe39
Octavio <>
2021-01-02 01:02:23 +0100

    Good random distribution

--- hellway.asm
+++ hellway.asm
@@ -336,27 +336,26 @@ DrawTraffic1; 17 Max, will be more
 	TYA; 2
 	CLC; 2 
 	ADC TrafficOffset1 + 1;3
-	STA Tmp0
+	AND #%11111000 ;2
+	TAX ;2
+	LDA AesTable,X ; 4
+	STA Tmp0 ;3
 	;AND #%00001000 ;2 Every 8 for now
 	;BEQ FinishDrawTrafficLine1 ;2
 	;LDA #%11000000 ;2
 	;STA PF2Cache ;3
 	LDA #0 ;2
 	ADC TrafficOffset1 + 2;3
-	STA Tmp1
-	LDA Tmp0
-	AND #%11111100 ; 4 lines same patern
-	ROL
-	ROL
-	EOR Tmp1
-	EOR #%10101010
-	CMP #$A0
-	BCC FinishDrawTraffic1
-	LDA #%11000000
+	TAX ;2
+	LDA AesTable,X ;4
+	EOR Tmp0 ;3
+	CMP #$30;2
+	BCS FinishDrawTraffic1 ; Greater or equal don't draw; 2 (no branch) or 3 (branch) or 4 (Branch cross page) 
+	LDA #%11000000 ;2
 	STA PF1Cache ;3
-FinishDrawTraffic1	
 
-FinishDrawTrafficLine1
+	;39 cyles worse case! 35 or 36 best case.
+FinishDrawTraffic1	
 
 	STA WSYNC ;49
 
@@ -407,6 +406,27 @@ OverScanWait
 	JMP  MainLoop      
 
 
+	org $FFE0
+AesTable
+	DC.B $63,$7c,$77,$7b,$f2,$6b,$6f,$c5,$30,$01,$67,$2b,$fe,$d7,$ab,$76
+	DC.B $ca,$82,$c9,$7d,$fa,$59,$47,$f0,$ad,$d4,$a2,$af,$9c,$a4,$72,$c0
+	DC.B $b7,$fd,$93,$26,$36,$3f,$f7,$cc,$34,$a5,$e5,$f1,$71,$d8,$31,$15
+	DC.B $04,$c7,$23,$c3,$18,$96,$05,$9a,$07,$12,$80,$e2,$eb,$27,$b2,$75
+	DC.B $09,$83,$2c,$1a,$1b,$6e,$5a,$a0,$52,$3b,$d6,$b3,$29,$e3,$2f,$84
+	DC.B $53,$d1,$00,$ed,$20,$fc,$b1,$5b,$6a,$cb,$be,$39,$4a,$4c,$58,$cf
+	DC.B $d0,$ef,$aa,$fb,$43,$4d,$33,$85,$45,$f9,$02,$7f,$50,$3c,$9f,$a8
+	DC.B $51,$a3,$40,$8f,$92,$9d,$38,$f5,$bc,$b6,$da,$21,$10,$ff,$f3,$d2
+	DC.B $cd,$0c,$13,$ec,$5f,$97,$44,$17,$c4,$a7,$7e,$3d,$64,$5d,$19,$73
+	DC.B $60,$81,$4f,$dc,$22,$2a,$90,$88,$46,$ee,$b8,$14,$de,$5e,$0b,$db
+	DC.B $e0,$32,$3a,$0a,$49,$06,$24,$5c,$c2,$d3,$ac,$62,$91,$95,$e4,$79
+	DC.B $e7,$c8,$37,$6d,$8d,$d5,$4e,$a9,$6c,$56,$f4,$ea,$65,$7a,$ae,$08
+	DC.B $ba,$78,$25,$2e,$1c,$a6,$b4,$c6,$e8,$dd,$74,$1f,$4b,$bd,$8b,$8a
+	DC.B $70,$3e,$b5,$66,$48,$03,$f6,$0e,$61,$35,$57,$b9,$86,$c1,$1d,$9e
+	DC.B $e1,$f8,$98,$11,$69,$d9,$8e,$94,$9b,$1e,$87,$e9,$ce,$55,$28,$df
+	DC.B $8c,$a1,$89,$0d,$bf,$e6,$42,$68,$41,$99,$2d,$0f,$b0,$54,$bb,$16
+
+; From FFF0 to FFFB (122 bytes) to use here
+
 CarSprite ; Upside down
 	.byte #%00000000 ; Easist way to stop drawing
 	.byte #%11111111
@@ -423,6 +443,7 @@ TrafficSpeeds ;maybe move to ram for dynamic changes of speed and 0 page access
 	.byte #$A0; Trafic1 L
 	.byte #0;   Trafic1 H
 
+
 	org $FFFC
-	.word Start
-	.word Start
+		.word Start
+		.word Start
